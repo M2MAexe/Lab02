daccbe8030a3806b0e4eae89d755265d674850d9
M2MAlove <93481311+M2MAlove@users.noreply.github.com>
2022-11-07 10:38:35 +0300

    Delete pokemonEelektross.java

--- pokemonEelektross.java
+++
@@ -1,8 +0,0 @@
-public class pokemonEelektross
-			extends pokemonEelektrik {
-	public pokemonEelektross(java.lang.String Name, int Level) {
-		super(Name, Level);	
-		setStats(85, 115, 80, 105, 80, 50);
-	}
-
-}
